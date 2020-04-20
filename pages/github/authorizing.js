c1c161066302a03bd52e2a76537c1a04042d98ff
Kendall Strautman <kendallstrautman@gmail.com>
2020-04-20 11:25:04 -0700

    step 4 - adds auth page

---
+++ pages/github/authorizing.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import { useGithubAuthRedirect } from 'react-tinacms-github'
+
+export default function Authorizing() {
+	// Let the main app know, that we receieved an auth code from the Github redirect
+	useGithubAuthRedirect()
+	return <h2>Authorizing with Github, Please wait...</h2>
+}
